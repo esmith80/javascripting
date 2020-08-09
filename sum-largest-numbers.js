6a70ae18f888f6917bcc6c96a8de3b7af15efa83
11em <Edward@Edwards-iMac.local>
2020-08-09 08:35:36 -0600

    added sum largest numbers

--- sum-largest-numbers.js
+++ sum-largest-numbers.js
@@ -1,21 +1,4 @@
 
-const sumLargestNumbers = function(data) {
-  let biggest = data[0];
-  let biggestIndex = -1;
-  for(let i = 0; i < 2; i++) { // 2 loops, 1st loop finds biggest number and records index of that number in data array
-    
-    for(let j = 0; j < data.length; j++) { 
-      if(biggestIndex === j) { // if 
-        continue;
-      } else if (biggest <= data[j]) {
-          biggest = data[j];
-          biggestIndex = j; // if data[0] is the biggest number, j will never be set 
-        }
-      }
-
-    }
-  };
-/*
 const sumLargestNumbers = (values) => { 
   let largestA = 0;
   let largestB = 0;
@@ -34,6 +17,6 @@ const sumLargestNumbers = (values) => {
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
-*/
+
 
 
